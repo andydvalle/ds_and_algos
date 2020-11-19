1cd766c0cb3cd0136a1eb3e451f4c7353d953389
github username <andydelvalle89@gmail.com>
2020-11-19 14:50:53 -0800

    add colt's solution

--- colt_steele/linear_search/linearSearch.js
+++ colt_steele/linear_search/linearSearch.js
@@ -1,4 +1,5 @@
-function linearSearch(arr, val) {
+// my solution, O(n) solution
+function linearSearch1(arr, val) {
   // add whatever parameters you deem necessary - good luck!
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === val) {
@@ -7,3 +8,14 @@ function linearSearch(arr, val) {
   }
   return -1;
 }
+
+// colt's solution, O(n) solution
+function linearSearch(arr, val) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === val) return i;
+  }
+}
+
+// test
+console.log(linearSearch1([34, 56, 1, 2], 1));
+console.log(linearSearch([34, 56, 1, 2], 1));
