a808311781b4f04fb0f38eef1304b0f1c245efc5
github username <andydelvalle89@gmail.com>
2020-11-20 07:38:23 -0800

    learn naive string ssearch

---
+++ colt_steele/searching_algorithms/naiveStringSearch.js
@@ -0,0 +1,22 @@
+//search for a smaller substring within a string
+
+//Pseudocode:
+// loop over the longer string
+// loop over the shorter string
+// if the characthers don't match, break out of the inner loop
+// if the characters do match, keep going
+// if you complete the inner loop and find a match, increment the count of matches
+// return the count
+
+function naiveSearch(long, short) {
+  let count = 0;
+  for (let i = 0; i < long.length; i++) {
+    for (let j = 0; j < short.length; j++) {
+      if (short[j] !== long[i + j]) break;
+      if (j === short.length - 1) count++;
+    }
+  }
+  return count;
+}
+
+console.log(naiveSearch("lorie loled", "pop"));
