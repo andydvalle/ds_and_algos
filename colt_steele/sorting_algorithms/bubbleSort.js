53dbb0046e86fd7ff8f5e6bb10e07cd74fc3db66
github username <andydelvalle89@gmail.com>
2020-11-20 10:56:53 -0800

    rearrange bubbleSort functions

--- colt_steele/sorting_algorithms/bubbleSort.js
+++ colt_steele/sorting_algorithms/bubbleSort.js
@@ -1,6 +1,11 @@
-// Bubble sort is a sorting algorithm where the largest values bubble up to the top
+/*
+  BUBBLE SORT is a sorting algorithm where the largest values bubble up to the top
+  Time complexity:
+  - worst case: O(n^2), generally
+  - best case: O(n), with noSwap optimization
+*/
 
-// Bubble sort less optimized version
+// BUBBLE SORT less optimized version
 function bubbleSort(arr) {
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length; j++) {
@@ -16,16 +21,15 @@ function bubbleSort(arr) {
   return arr;
 }
 
-// bubbleSort([37, 45, 29, 8]);
-// console.log("")
+/*
+  BUBBLE SORT optimized version Pseudocode:
+  - Start looping from the end of the array twoards the beginning, with a variable called i
+  - Start an inner loop with a variable called j from the beginning until i-1
+  - If arr[j] is greater than arr[j+1], swap those two values
+  - return the sorted array
+*/
 
-// Optimized Bubble Sort Pseudocode:
-// Start looping from the end of the array twoards the beginning, with a variable called i
-// Start an inner loop with a variable called j from the beginning until i-1
-// if arr[j] is greater than arr[j+1], swap those two values
-// return the sorted array
-
-// optimzed bubbleSort with ES5
+//  BUBBLE SORT optimzed with ES5
 function bubbleSortOpt1(arr) {
   for (let i = arr.length; i > 0; i--) {
     for (let j = 0; j < i - 1; j++) {
@@ -41,10 +45,7 @@ function bubbleSortOpt1(arr) {
   return arr;
 }
 
-console.log(bubbleSortOpt1([37, 45, 29, 8]));
-console.log("");
-
-// optimzed bubble sort with ES6
+// BUBBLE SORT optimzed with ES6
 const bubbleSortOpt2 = (arr) => {
   for (let i = arr.length; i > 0; i--) {
     for (let j = 0; j < i - 1; j++) {
@@ -58,4 +59,29 @@ const bubbleSortOpt2 = (arr) => {
   return arr;
 };
 
-console.log(bubbleSortOpt2([37, 45, 29, 8]));
+// Optimized with noSwaps
+const bubbleSortOpt3 = (arr) => {
+  let noSwaps;
+  for (let i = arr.length; i > 0; i--) {
+    noSwaps = true;
+    for (let j = 0; j < i - 1; j++) {
+      console.log(arr, arr[j], arr[j + 1]);
+      if (arr[j] > arr[j + 1]) {
+        //SWAP!
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        noSwaps = false;
+      }
+    }
+    if (noSwaps) break;
+  }
+  return arr;
+};
+
+// console.log(bubbleSort([37, 45, 29, 8]));
+// console.log("")
+// console.log(bubbleSortOpt1([37, 45, 29, 8]));
+// console.log("");
+// console.log(bubbleSortOpt2([37, 45, 29, 8]));
+// console.log("");
+// console.log(bubbleSortOpt3([4, 1, 2, 3]));
+// console.log("");
